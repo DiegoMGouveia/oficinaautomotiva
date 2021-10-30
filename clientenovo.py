d7e5df675af08b117e03473998b63d4041286014
DIEGO MALTA GOUVEIA <diego.pel@hotmail.com>
2021-10-30 01:24:34 -0300

    update no arquivo clientenovo.py
    adicionado caracteres na print (linha 42) para alinhar as informações.

--- clientenovo.py
+++ clientenovo.py
@@ -39,18 +39,18 @@ class Cliente:
     def InfoCliente(self):
         limpa()
         print('-'*35)
-        print(f'Nome do Cliente: {self.cliente}\n'
-              f'Contato: {self.contato}\n'
-              f'Endereço: {self.endereco}\n'
-              f'Placa: {self.placa}\n'
-              f'Marca/Modelo: {self.modelo}\n'
-              f'Cor: {self.cor}\n'
-              f'Ano: {self.ano}\n'
-              f'Tipo de serviço: {self.tipodeservico}\n'
-              f'Custo: {self.custo}\n'
-              f'Pagamento: {self.pago}\n'
-              f'Data: {self.data}\n'
+        print(f'Nome do Cliente.: {self.cliente}\n'
+              f'Contato.........: {self.contato}\n'
+              f'Endereço........: {self.endereco}\n'
+              f'Placa...........: {self.placa}\n'
+              f'Marca/Modelo....: {self.modelo}\n'
+              f'Cor.............: {self.cor}\n'
+              f'Ano.............: {self.ano}\n'
+              f'Tipo de serviço.: {self.tipodeservico}\n'
+              f'Custo...........: {self.custo}\n'
+              f'Pagamento.......: {self.pago}\n'
+              f'Data............: {self.data}\n'
               f'Prazo de entrega: {self.prazo}\n'
-              f'Observações: {self.observacao}\n')
+              f'Observações.....: {self.observacao}\n')
         print('-'*35)
 
