edd048bfc4955ee841ccff8232cf9a837efcf38c
DIEGO MALTA GOUVEIA <diego.pel@hotmail.com>
2021-10-31 01:01:41 -0300

    enviado arquivo bancodedados.py
    arquivo contem funções de conexão com o banco de dados sqlite3 e função para criar uma tabela

---
+++ bancodedados.py
@@ -0,0 +1,45 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def ConexaoBanco():
+    caminho = "/media/diego/EA00FB8D00FB5F4F/python3 projetos/lojacarros/basededadosoficina.db"
+    con = None
+    try:
+        con = sqlite3.connect(caminho)
+    except Error as ex:
+        print(ex)
+    return con
+
+vcon = ConexaoBanco()
+
+novadb = False # True vai cria um novo banco de dados - False caso ja tenha um banco de dados criado.
+if novadb == True:
+    vsql = '''CREATE TABLE clientesdb(
+                NOME VARCHAR,
+                CONTATO VARCHAR,
+                ENDERECO VARCHAR,
+                PLACA VARCHAR,
+                MODELO VARCHAR,
+                COR VARCHAR,
+                ANO VARCHAR,
+                TIPODESERVICO VARCHAR,
+                OBERVACAO VARCHAR,
+                CUSTO VARCHAR,
+                PAGO VARCHAR
+            );'''
+
+    def CriarTabela(conexao,sql):
+            try:
+                c=conexao.cursor()
+                c.execute(sql)
+                print('tabela criada!')
+            except Error as ex:
+                print(ex)
+
+
+    CriarTabela(vcon, vsql)
+
+
+    vcon.close()
+
